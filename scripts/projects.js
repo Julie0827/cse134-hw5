cd74f450f2b2e87a68836747346bbf76bd726139
Julie0827 <jackjack170827@gmail.com>
2025-03-11 18:44:54 -0700

    Add more project data & dynamic anchors ⚓️

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,14 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loadLocal = document.getElementById("load-local");
     const projectsContainer = document.getElementById("projects");
+    const projectNav = document.getElementById("project-nav");
     
     loadLocal.addEventListener("click", function () { 
         projectsContainer.innerHTML = "";
+        projectNav.innerHTML = "";
+
         const storedProjects = JSON.parse(localStorage.getItem("projects")) || [];
 
         storedProjects.forEach(project => {
             const card = new ProjectCard(project);
+            const anchor = document.createElement("a");
+
+            anchor.href = `#${project.id}`;
+            anchor.textContent = project.title;
+
             projectsContainer.appendChild(card);
+            projectNav.appendChild(anchor);
         });
     });
 });
